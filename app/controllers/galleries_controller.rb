e6cfbb960509e2353050c926c0d627df426b3b2a
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2010-08-11 23:58:03 -0500

    Refactoring galleries controller

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,72 +1,40 @@
 class GalleriesController < ApplicationController
+  respond_to :html
   # GET /galleries
   # GET /galleries.xml
   def index
-    @galleries = Gallery.all.paginate :page => params[:page] || 1, :per_page => params[:per_page] || 5
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @galleries }
-    end
+    respond_with(@galleries = Gallery.all.paginate(:page => params[:page] || 1, :per_page => params[:per_page] || 5))
   end
 
   # GET /galleries/1
   # GET /galleries/1.xml
   def show
-    @gallery = Gallery.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @gallery }
-    end
+    respond_with(@gallery = Gallery.find(params[:id]))
   end
 
   # GET /galleries/new
   # GET /galleries/new.xml
   def new
-    @gallery = Gallery.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @gallery }
-    end
+    respond_with(@gallery = Gallery.new)
   end
 
   # GET /galleries/1/edit
   def edit
-    @gallery = Gallery.find(params[:id])
+    respond_with(@gallery = Gallery.find(params[:id]))
   end
 
   # POST /galleries
   # POST /galleries.xml
   def create
-    @gallery = Gallery.new(params[:gallery])
-
-    respond_to do |format|
-      if @gallery.save
-        format.html { redirect_to(@gallery, :notice => 'Gallery was successfully created.') }
-        format.xml  { render :xml => @gallery, :status => :created, :location => @gallery }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @gallery.errors, :status => :unprocessable_entity }
-      end
-    end
+    respond_with(@gallery = Gallery.create(params[:gallery]), :status => :created)
   end
 
   # PUT /galleries/1
   # PUT /galleries/1.xml
   def update
     @gallery = Gallery.find(params[:id])
-
-    respond_to do |format|
-      if @gallery.update_attributes(params[:gallery])
-        format.html { redirect_to(@gallery, :notice => 'Gallery was successfully updated.') }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @gallery.errors, :status => :unprocessable_entity }
-      end
-    end
+    @gallery.update_attributes(params[:gallery])
+    respond_with(@gallery, :status => :updated)
   end
 
   # DELETE /galleries/1
@@ -74,10 +42,6 @@ class GalleriesController < ApplicationController
   def destroy
     @gallery = Gallery.find(params[:id])
     @gallery.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(galleries_url) }
-      format.xml  { head :ok }
-    end
+    respond_with(@gallery, :status => :deleted, :location => galleries_path)
   end
 end
